7494216a43c659de7f86713de18736267ad98fad
Asher Norland <asher.norland@gmail.com>
2022-05-24 16:59:00 -0400

    Refresh Template

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <!-- Remove this component to get started! -->
-    <NuxtWelcome />
+  <div class="sm:py-10">
+    <Html lang="en" />
+    <Meta property="og:image" content="/cover.jpg" />
+    <Meta name="twitter:card" content="summary_large_image" />
+    <Navbar />
+    <NuxtPage />
   </div>
 </template>
+
+<style lang="postcss">
+body {
+  @apply bg-gray-50 dark:bg-gray-900;
+}
+</style>
