a1927241edf0e7d2226735fc7067509ae2dbaa21
Asher Norland <asher.norland@gmail.com>
2022-06-12 16:43:58 -0400

    Remove Unused Footer Info

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,24 +1,10 @@
 <template lang="pug">
 footer
   small Website made with ❤️ by Asher and Ola
-  small Copyright &copy; {{ new Date().getFullYear() }},
-    strong &nbsp; {{ copyright }}
+  br
+  small Copyright &copy; {{ new Date().getFullYear() }}
 </template>
 
-<script>
-export default {
-  data: () => ({
-    copyright: '',
-    icons: [
-      // TODO: Fill with `social` datatype
-    ],
-    gravatar: ''
-  }),
-  async fetch() {
-    const config = await this.$content('config').fetch()
+<script setup>
 
-    this.gravatar = `https://www.gravatar.com/avatar/${config.gravater_hash}?size=256r=g`
-    this.copyright = config.copyright
-  }
-}
 </script>
