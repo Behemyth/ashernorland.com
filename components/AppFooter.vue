972b9f760b3f1195bc7b6ab8e6540fbd7f8aa707
Asher Norland <asher.norland@gmail.com>
2022-06-12 12:26:28 -0400

    Strip Footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,34 +1,8 @@
 <template lang="pug">
 footer
-  container
-    row
-      v-spacer
-      v-btn(to='/', fab, nuxt, icon)
-        v-avatar(size='48')
-          v-img(
-            :src='gravatar',
-            alt='Icon'
-          )
-      v-spacer
-    row
-      v-spacer
-      v-btn(
-        v-for='(item, index) in icons',
-        :key='index',
-        icon,
-        :href='item.url'
-      )
-        v-icon(large) {{ item.icon }}
-      v-spacer
-    row
-      v-spacer
-      small Website made with ❤️ by Asher and Ola
-      v-spacer
-    row
-      v-spacer
-      small Copyright &copy; {{ new Date().getFullYear() }},
-        strong &nbsp; {{ copyright }}
-      v-spacer
+  small Website made with ❤️ by Asher and Ola
+  small Copyright &copy; {{ new Date().getFullYear() }},
+    strong &nbsp; {{ copyright }}
 </template>
 
 <script>
